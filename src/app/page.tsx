bb73105eb09f5170764140acdeba0cca7d2b074e
Mubaraq Mustapha <ayomidemusty@gmail.com>
2023-07-27 13:40:18 +0100

    feat: add more sections

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,18 @@
 import Image from "next/image";
 import Navbar from "./components/Navbar";
 import headerImg from "@/assets/AgenSQL-Logo.png";
+import enterprise from "@/assets/enterprise.png";
+import features from "@/assets/features.png";
+import password from "@/assets/password.png";
+import data from "@/assets/data.png";
+import auditing from "@/assets/auditing.png";
+import replication from "@/assets/replication.png";
 
 export default function Home() {
   return (
     <>
       <Navbar />
-      <main>
+      <main className="pt-[70px]">
         <section className="pt-[70px] pb-[24px] sm:pb-[40px] lg:pb-[64px] text-center flex flex-col items-center gap-[15px] lg:gap-[30px] text-[#444444] px-[3%]">
           <div>
             <Image src={headerImg} alt="header" />
@@ -43,6 +49,71 @@ export default function Home() {
               blog
             </p>
           </div>
+        </section>
+
+        <section className="bg-[#f3f6fb] py-[60px] flex flex-col items-center justify-center">
+          <div className="w-[90%] lg:w-[70%] mx-[auto]">
+            <Image src={enterprise} alt="enterprise" />
+          </div>
+        </section>
+
+        <section className="px-[3%] py-[80px] flex flex-col items-center justify-center text-center gap-[30px]">
+          <h2 className="font-bold text-[24px] sm:text-[34px]">Key Features</h2>
+          <p>
+            Manage your data with Agens Enterprise Package
+            <br />
+            Essential enterprise features such as high availability and sharding
+            are provided
+          </p>
+          <div className="w-[90%] lg:w-[70%] mx-[auto]">
+            <Image src={features} alt="features" />
+          </div>
+        </section>
+
+        <section className="bg-[#f3f6fb] px-[3%] py-[80px] flex flex-col items-center justify-center text-center gap-[30px]">
+          <h2 className="font-bold text-[24px] sm:text-[34px]">
+            AgensSQL DB Engine
+          </h2>
+          <h4 className="text-[#444444] underline text-[18px] font-bold">
+            Enhanced Data Security
+          </h4>
+          <p>
+            AgensSQL is a PostgreSQL-based DBMS
+            <br />
+            that guarantees optimal security and stability.
+          </p>
+          <div className="flex flex-col sm:flex-row items-center justify-center gap-[30px] sm:gap-[5%]">
+            <div className="w-[90%] sm:w-[30%] lg:w-[25%] bg-white px-[60px] py-[40px] rounded-[5px] border-solid border border-[#cccccc]">
+              <Image src={password} alt="password" />
+            </div>
+            <div className="w-[90%] sm:w-[30%] lg:w-[25%] bg-white px-[60px] py-[40px] rounded-[5px] border-solid border border-[#cccccc]">
+              <Image src={data} alt="data" />
+            </div>
+            <div className="w-[90%] sm:w-[30%] lg:w-[25%] bg-white px-[60px] py-[40px] rounded-[5px] border-solid border border-[#cccccc]">
+              <Image src={auditing} alt="auditing" />
+            </div>
+          </div>
+        </section>
+
+        <section className="px-[3%] py-[80px] flex flex-col items-center justify-center text-center gap-[30px]">
+          <h2 className="font-bold text-[24px] sm:text-[34px]">
+            Agens Enterprise Package
+          </h2>
+          <p>
+            Agens Enterprise Package comes with a high availability management
+            server that
+            <br />
+            supports backup and data monitoring dashboard that helps enterprise
+            customers manage their data efficiently.
+          </p>
+          <div className="flex flex-col sm:flex-row justify-center gap-[30px] sm:gap-[5%]">
+            <div>
+              <div className="w-[90%] lg:w-[70%] mx-[auto]">
+                <Image src={replication} alt="replication" />
+              </div>
+            </div>
+          </div>
+          <div></div>
         </section>
       </main>
     </>
